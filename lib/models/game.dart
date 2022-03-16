7e7f1cac315982cc430e0481834195def2399312
abdullakn <abdudebanjazz@gmail.com>
2022-03-16 15:44:22 +0530

    game model added

---
+++ lib/models/game.dart
@@ -0,0 +1,69 @@
+class Game {
+  String bgImg;
+  String icon;
+  String name;
+  String type;
+  num score;
+  num downloads;
+  num review;
+  String desc;
+  List<String> imgs;
+
+  Game(
+      {required this.bgImg,
+      required this.icon,
+      required this.name,
+      required this.type,
+      required this.score,
+      required this.downloads,
+      required this.review,
+      required this.desc,
+      required this.imgs});
+
+  static List<Game> generateGame() {
+    return [
+      Game(
+          bgImg: "assets/images/ori1.jpg",
+          icon: "assets/images/ori_logo.jpg",
+          name: "Ori and the Blind Forest",
+          type: "Adventure",
+          score: 4.5,
+          downloads: 354,
+          review: 200,
+          desc: "The beautiful game, every kids should play this once",
+          imgs: [
+            "assets/images/ori1.jpg",
+            "assets/images/ori2.jpg",
+            "assets/images/ori3.jpg"
+          ]),
+      Game(
+          bgImg: "assets/images/ori1.jpg",
+          icon: "assets/images/ori_logo.jpg",
+          name: "Ori and the Blind Forest",
+          type: "Adventure",
+          score: 4.5,
+          downloads: 354,
+          review: 200,
+          desc: "The beautiful game, every kids should play this once",
+          imgs: [
+            "assets/images/ori1.jpg",
+            "assets/images/ori2.jpg",
+            "assets/images/ori3.jpg"
+          ]),
+      Game(
+          bgImg: "assets/images/ori1.jpg",
+          icon: "assets/images/ori_logo.jpg",
+          name: "Ori and the Blind Forest",
+          type: "Adventure",
+          score: 4.5,
+          downloads: 354,
+          review: 200,
+          desc: "The beautiful game, every kids should play this once",
+          imgs: [
+            "assets/images/ori1.jpg",
+            "assets/images/ori2.jpg",
+            "assets/images/ori3.jpg"
+          ])
+    ];
+  }
+}
