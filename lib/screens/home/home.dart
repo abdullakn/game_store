34b261fd149694b0872a22f7369311acbb9fb511
abdullakn <abdudebanjazz@gmail.com>
2022-03-17 11:23:48 +0530

    home page bottom nav completed

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -13,8 +13,8 @@ class HomePage extends StatelessWidget {
       backgroundColor: kPrimaryColor,
       body: SafeArea(
           child: SingleChildScrollView(
-            child: Stack(
-                  children: [
+        child: Stack(
+          children: [
             Transform(
               transform: Matrix4.identity()..rotateZ(20),
               origin: Offset(150, 50),
@@ -38,9 +38,59 @@ class HomePage extends StatelessWidget {
             Column(
               children: [Header(), Seacrh(), CategoryGame()],
             )
-                  ],
-                ),
-          )),
+          ],
+        ),
+      )),
+      bottomNavigationBar: _buildBottomNavigationBar(),
     );
+  }
+
+  Widget _buildBottomNavigationBar() {
+    return Container(
+      color: Colors.white,
+      child: Container(
+        decoration: BoxDecoration(
+         
+
+            // borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20)),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.2),
+                  spreadRadius: 5,
+                  blurRadius: 10),
+            ]),
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+          child: BottomNavigationBar(
+            selectedItemColor: kPrimaryColor,
+            unselectedItemColor: Colors.grey.withOpacity(0.3),
+            selectedFontSize: 12,
+            unselectedFontSize: 12,
+            type: BottomNavigationBarType.fixed,
+            items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+            _buildItem('Application', Icons.more_horiz_outlined),
+            _buildItem('Film', Icons.play_arrow_rounded),
+            _buildItem('Book', Icons.auto_stories_rounded)
+            // BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          ]),
+        ),
+      ),
+    );
+  }
+
+  BottomNavigationBarItem _buildItem(String text,IconData icon){
+    return BottomNavigationBarItem(label: text,icon: Container(
+      margin: EdgeInsets.only(bottom: 5),
+      padding: EdgeInsets.all(5),
+      decoration:BoxDecoration(
+        color: Colors.grey.withOpacity(0.2),
+        borderRadius: BorderRadius.circular(10),
+
+
+      ),
+      child: Icon(icon,size: 30,color: Colors.grey,),
+    ));
   }
 }
